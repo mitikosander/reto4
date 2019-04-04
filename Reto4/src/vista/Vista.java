32c0ec26aaa45d7c97d182940d9990686b39d297
elorrietaa <alba.rodriguezsa@elorrieta-errekamari.com>
2019-04-04 09:36:44 +0200

    Añadido metodo para cambiar de una pantalla a otra

--- Reto4/src/vista/Vista.java
+++ Reto4/src/vista/Vista.java
@@ -1,10 +1,23 @@
 package vista;
 
+import javax.swing.JPanel;
+
 public class Vista {
 //Aqui crear la instancia en privado de los objetos de cada clase
+	private Contenedor ventana;
+	private Inicio inicio;
+	
 	
 	public Vista() {
 		//Aqui completamos la instancia del objeto para poder acceder a sus atributos
+		ventana=new Contenedor();
+		inicio=new Inicio();
+	}
+	
+	public  void mostrarPantalla(JPanel panel) {
+		ventana.frame.setContentPane(panel);
+		ventana.frame.setVisible(true);
+		
 	}
 	
 }
