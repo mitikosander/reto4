b5ecaa6576fee515f00eb60e5a0707eaaee93b90
in1dm3b_18 <in1dm3b_18@HZ317814.H015112.NET>
2019-04-08 11:51:49 +0200

    Mostrar lista pantalla creada para que tito haga magia en ella

---
+++ Reto4/src/vista/Mostrar_Lista.java
@@ -0,0 +1,16 @@
+package vista;
+
+import javax.swing.JPanel;
+import java.awt.BorderLayout;
+
+public class Mostrar_Lista extends JPanel {
+
+	/**
+	 * Create the panel.
+	 */
+	public Mostrar_Lista() {
+		setLayout(null);
+
+	}
+
+}
--- Reto4/src/vista/Pago.java
+++ Reto4/src/vista/Pago.java
@@ -1,4 +1,4 @@
-package vista;
+	package vista;
 
 import javax.swing.JPanel;
 
