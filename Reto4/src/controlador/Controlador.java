237a800aa322504124843ce7705b90882b33bbcc
alain <alain@DESKTOP-QC600PS>
2019-04-11 13:50:06 +0200

    Cambio de conexion BBDD, creado fitxero con las creenciales, acciones de botones hasta la pantalla pagar, acciones de los botones del dinero. Alain

--- Reto4/src/controlador/Controlador.java
+++ Reto4/src/controlador/Controlador.java
@@ -12,6 +12,9 @@ public class Controlador {
 	private static Vista vista;
 	private Modelo modelo;
 	private ArrayList<modelo.Alojamiento> alojamientos;
+	public double TotalaPagar =0;
+	public double Adevolver =0;
+	public double Introducido= 0;
 	public Controlador(Vista vista, Modelo modelo) {
 		Controlador.vista = vista;
 		this.modelo = modelo;
@@ -29,31 +32,184 @@ public class Controlador {
 
 		vista.getInicio().getBtn_buscar_inicio().addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
-				alojamientos=modelo.getMetodos().buscarAlojamientos();
+				//Tras la busqueda inicial vamos a la pantall de seleccion de hoteles
+				vista.mostrarPantalla(vista.getListahoteles());
+			
+			}
+		});
+		
+		vista.getListahoteles().getBtnaceptar_mostrar_lista().addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
 				//Tras la busqueda inicial vamos a la pantall de seleccion de hoteles
 				vista.mostrarPantalla(vista.getPagar());
 			
 			}
 		});
 		
-		//desde la pantalla de seleccion de hoteles cargamos el precio desde un metodo en modelo,
-		//y pasamos ese valor a la pantalla de pago para proceder a este
+		vista.getListahoteles().getBtnCancelar_mostrar_lista().addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				//Tras la busqueda inicial vamos a la pantall de seleccion de hoteles
+				vista.mostrarPantalla(vista.getInicio());
+			
+			}
+		});
+		vista.getPagar().getbtn50euro().addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				// Introducimos 50 eurazos
+				if (Introducido>=TotalaPagar) {
+					Adevolver= Introducido-TotalaPagar;
+				}else {
+					Introducido = Introducido + 50;
+				}
+			}
+		});
+		vista.getPagar().getbtn20euro().addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				// Introducimos 20 eurazos
+				if (Introducido>=TotalaPagar) {
+					Adevolver= Introducido-TotalaPagar;
+				}else {
+					Introducido = Introducido + 20;
+				}
+			}
+		});
+		vista.getPagar().getbtn10euro().addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				// Introducimos 10 eurazos
+				if (Introducido>=TotalaPagar) {
+					Adevolver= Introducido-TotalaPagar;
+				}else {
+					Introducido = Introducido + 10;
+				}
+			}
+		});
+		vista.getPagar().getbtn5euro().addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				// Introducimos 5 eurazos
+				if (Introducido>=TotalaPagar) {
+					Adevolver= Introducido-TotalaPagar;
+				}else {
+					Introducido = Introducido + 5;
+				}
+			}
+		});
+		vista.getPagar().getbtn2euro().addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				// Introducimos 2 eurazos
+				if (Introducido>=TotalaPagar) {
+					Adevolver= Introducido-TotalaPagar;
+				}else {
+					Introducido = Introducido + 2;
+				}
+			}
+		});
+		vista.getPagar().getbtn1euro().addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				// Introducimos 1 eurazo
+				if (Introducido>=TotalaPagar) {
+					Adevolver= Introducido-TotalaPagar;
+				}else {
+					Introducido = Introducido + 1;
+				}
+			}
+		});
+		vista.getPagar().getbtn50cnt().addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				// Introducimos 50 centimazos
+				if (Introducido>=TotalaPagar) {
+					Adevolver= Introducido-TotalaPagar;
+				}else {
+					Introducido = Introducido + 0.50;
+				}
+			}
+		});
+		vista.getPagar().getbtn20cnt().addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				// Introducimos 20 centimazos
+				if (Introducido>=TotalaPagar) {
+					Adevolver= Introducido-TotalaPagar;
+				}else {
+					Introducido = Introducido + 0.20;
+				}
+			}
+		});
+		vista.getPagar().getbtn10cnt().addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				// Introducimos 51 centimazos
+				if (Introducido>=TotalaPagar) {
+					Adevolver= Introducido-TotalaPagar;
+				}else {
+					Introducido = Introducido + 0.10;
+				}
+			}
+		});
+		vista.getPagar().getbtn5cnt().addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				// Introducimos 5 centimazos
+				if (Introducido>=TotalaPagar) {
+					Adevolver= Introducido-TotalaPagar;
+				}else {
+					Introducido = Introducido + 0.05;
+				}
+			}
+		});
+		vista.getPagar().getbtn2cnt().addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				// Introducimos 2 centimazos
+				if (Introducido>=TotalaPagar) {
+					Adevolver= Introducido-TotalaPagar;
+				}else {
+					Introducido = Introducido + 0.02;
+				}
+			}
+		});
+		vista.getPagar().getbtn1cnt().addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				// Introducimos 1centimazo
+				if (Introducido>=TotalaPagar) {
+					Adevolver= Introducido-TotalaPagar;
+				}else {
+					Introducido = Introducido + 0.01;
+				}
+			}
+		});
+	}
 		
 		
 		
 		
+		
+	
+	public void SumarIntroducido(double cantidad) {
+		double TotalIntroducido = 0;
+		double cambios=0;
+		double Total=0;
+		TotalIntroducido = TotalIntroducido + cantidad;
+		vista.getPagar().gettextField_1().setText(Double.toString(TotalIntroducido));
+		// Datos.sacarResto=Datos.Total-Datos.TotalIntroducido;
+		if (Total > TotalIntroducido) {
+			vista.getPagar().gettextField_2().setText(Double.toString(0));
+
+		} else {
+			vista.getPagar().gettextField_2().setText(Double.toString(TotalIntroducido - Total));
+			cambios = TotalIntroducido - Total;
+		}
 	}
 	
 	//Metodo para rellenar el combobox con los datos consultados a la BBDD
 	
 	private void rellenarComboUbicaciones() {
+		//Sacar las lineas de la BBDD y rellenar el combobox
+		//1.Sacar datos de la BBDD
+				
+		ArrayList<String> ciudades=modelo.getMetodos().cargarCiudades();
+		
+		//2.Rellenar combo de lineas		
 
-		ArrayList<String> nombreParadas = modelo.getMetodos().cargarciudades();
-
-		// Rellenar las paradas
-		for (int i = 0; i < nombreParadas.size(); i++) {
-			vista.getInicio().getCombo_ubicacion().addItem(nombreParadas.get(i));
+		for(int i = 0;i<ciudades.size();i++) {
+			vista.getInicio().getCombo_ubicacion().addItem(ciudades.get(i));
 		}
+
 	}
 	
 	//Metodo para rellenar la tabla con los nombres y precios de los hoteles
--- Reto4/src/modelo/BBDD.java
+++ Reto4/src/modelo/BBDD.java
@@ -1,21 +1,22 @@
 package modelo;
-
+ 
+	import java.beans.Statement;
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.sql.Connection;
-import java.sql.DriverManager;
+	import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
-
+	 
 public class BBDD {
-    //Declaramos los datos para la conexion
-    static String passBBDD="koldo";
-    static String userBBDD="koldo";
-    final static String NOMBREBBDD="reto4";
-    final static String IPBBDD="127.0.0.1";
-    static String rutaBBDD="jdbc:mysql:/"+IPBBDD+"/"+NOMBREBBDD+"?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
-    
-    private Connection conexion;
-    
-    //Creamos el metodo para conectar a la base
-    public Connection conectarBase() {
+	
+	private Connection conexion;
+	
+	//Metodo al que llamaremos para inicializar la conexion con la base de datos
+	public Connection conectarBase() {
+		
+		//Variables para la conexion
+		String usuario=Fitxero.user,password=Fitxero.pass,url=Fitxero.URL;
 		
 		//llamamos al Driver
 		try {
@@ -24,34 +25,39 @@ public class BBDD {
 		} catch (ClassNotFoundException e) {
 			// TODO Auto-generated catch block
 			System.err.println("Driver no encontrado");
+			e.printStackTrace();
 		}
 		
 		//establecemos la conexion pasando los parametros definidos
 		try {
 			 conexion = DriverManager.getConnection(
-					   rutaBBDD,
-					   userBBDD,
-					   passBBDD);
+					   url,
+					   usuario,
+					   password);
 			System.out.println("Conexion completa");
 			return conexion;
 			
 		} catch (SQLException e) {
-			System.err.println("Error en la conexion" +e);
+			System.err.println("Error en la conexion");
+			e.printStackTrace();
 			return null;
 		}
 	
 		
 	}
-    
-  //Método para desconectar la BBDD
 	
-  	public void desconectar() {
-          if(conexion != null) {
-              try {
-                  conexion.close();
-                  conexion = null;
-              } catch (SQLException e) {
-              }
-          }
-      }
+	
+	//Método para desconectar la BBDD
+	
+	public void desconectar() {
+        if(conexion != null) {
+            try {
+                conexion.close();
+                conexion = null;
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
+	 
