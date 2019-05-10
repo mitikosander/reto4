af06af3720b3aa31dc62a4774ff314ac715a6a83
IN1DM3B_18 <IN1DM3B_18@HZ317814.H015112.NET>
2019-05-10 09:30:14 +0200

    Pantalla datos de personas

--- Reto4/src/vista/Datos_Personas.java
+++ Reto4/src/vista/Datos_Personas.java
@@ -2,6 +2,9 @@ package vista;
 
 import javax.swing.JPanel;
 import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import com.toedter.calendar.JDateChooser;
 
 
 public class Datos_Personas extends JPanel {
@@ -10,6 +13,9 @@ public class Datos_Personas extends JPanel {
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
+	private JTextField textField;
+	private JTextField textField_1;
+	private JTextField textField_2;
 
 	/**
 	 * Create the panel.
@@ -20,6 +26,41 @@ public class Datos_Personas extends JPanel {
 		JButton btnContinuar_dtpersonas = new JButton("Continuar");
 		btnContinuar_dtpersonas.setBounds(351, 266, 89, 23);
 		add(btnContinuar_dtpersonas);
+		
+		JLabel lblNombre = new JLabel("Nombre: ");
+		lblNombre.setBounds(128, 28, 57, 14);
+		add(lblNombre);
+		
+		textField = new JTextField();
+		textField.setBounds(128, 43, 86, 20);
+		add(textField);
+		textField.setColumns(10);
+		
+		JLabel lblApellido = new JLabel("Apellido:");
+		lblApellido.setBounds(128, 68, 46, 14);
+		add(lblApellido);
+		
+		textField_1 = new JTextField();
+		textField_1.setBounds(128, 86, 86, 20);
+		add(textField_1);
+		textField_1.setColumns(10);
+		
+		JLabel lblDni = new JLabel("DNI:");
+		lblDni.setBounds(128, 117, 46, 14);
+		add(lblDni);
+		
+		textField_2 = new JTextField();
+		textField_2.setBounds(128, 131, 86, 20);
+		add(textField_2);
+		textField_2.setColumns(10);
+		
+		JLabel lblFechaDeNacimiento = new JLabel("Fecha de nacimiento: ");
+		lblFechaDeNacimiento.setBounds(128, 162, 105, 14);
+		add(lblFechaDeNacimiento);
+		
+		JDateChooser dateChooser = new JDateChooser();
+		dateChooser.setBounds(128, 189, 95, 20);
+		add(dateChooser);
 
 	}
 }
